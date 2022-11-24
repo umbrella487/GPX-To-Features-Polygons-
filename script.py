b4b812ac81203c1f8c2d56abc41247f5bffb8d87
umbrella487 <manhunt1565@gmail.com>
2022-11-24 00:17:49 +0000

    Optimized for speed

--- script.py
+++ script.py
@@ -1,102 +1,54 @@
-from genericpath import exists
 import arcpy
 import os
 import ConversionUtils
 
-def wks(path):
-    return os.path.dirname(path)
-
-def Feat_shp(path):
-    return '.shp' if os.path.basename(path).endswith('.shp') else ''
-
-
-expr = "fixName(!Name!)"
-
-codeblock = """def fixName(val):
-                    return str(val).split('.')[0]"""
-
-def getFileName(path):
+def fName(path):
     bname = os.path.basename(path).split('.')[0]
     for x in bname:
-        if x == '-':
-            bname = bname.replace('-','')
+        if x in ['-','&',]:
+            bname = bname.replace(x,'_')
     return bname
 
 def cleanUp(feature):
     arcpy.Delete_management(feature)
 
-def attachRasterFiles(Raster_Files_Location, Feature_name_desc_col, Feature):
-    arcpy.SetProgressorLabel('Creating Raster Catalog.......')
-    arcpy.AddMessage('Creating Raster Catalog.......')
-    arcpy.CreateRasterCatalog_management(wks(Feature), 'CatLog')
-
-    arcpy.SetProgressorLabel('Copying Certificates to RasterCatalog......')
-    arcpy.WorkspaceToRasterCatalog_management(Raster_Files_Location, os.path.join(wks(Feature), 'CatLog'))
-
-    arcpy.SetProgressorLabel('Adding Name field to RasterCatalog......')
-    arcpy.AddField_management(os.path.join(wks(Feature), 'CatLog'), 'Name_0', 'TEXT')
-
-    arcpy.SetProgressorLabel('Calculating Name field to RasterCatalog......')
-    arcpy.CalculateField_management(os.path.join(wks(Feature), 'CatLog'), 'Name_0', expr, 'PYTHON', codeblock)
-
-    arcpy.SetProgressorLabel('Attaching Certificates to {}......'.format(os.path.basename(Feature)))
-    arcpy.AddMessage('Attaching Certificates to {}......'.format(os.path.basename(Feature)))
-    arcpy.JoinField_management(Feature, Feature_name_desc_col, os.path.join(wks(Feature), 'CatLog'), 'Name_0')
-    arcpy.DeleteField_management(Feature, 'Name_1')
-    arcpy.DeleteField_management(Feature, 'Name_0')
-    arcpy.Delete_management(os.path.join(wks(Feature), 'CatLog'))
-
-def gpxtoPolygon(gpxFiles, name_desc_col, coord_sys, outputFeature,area_condition='',area_unit='', 
-                    RasterAttachment_condition='',RasterFiles_Location=''):
+def gpxtoPolygon(gpxFiles, name_desc_col, outputFeature, computeArea='', areaUnit=''):
     try:
         polygons={}
         gpxFiles = ConversionUtils.SplitMultiInputs(gpxFiles)
         for gpxfile in gpxFiles:
-            arcpy.GPXtoFeatures_conversion(gpxfile, os.path.join(wks(outputFeature), 
-                                        '{}_points{}'.format(getFileName(gpxfile),Feat_shp(outputFeature))))
-
-            arcpy.Project_management(os.path.join(wks(outputFeature),'{}_points{}'.format(getFileName(gpxfile),Feat_shp(outputFeature))),
-                                        os.path.join(wks(outputFeature), '{}_points_proj{}'.format(getFileName(gpxfile),Feat_shp(outputFeature))),
-                                        coord_sys,
-                                        '',arcpy.SpatialReference(4326))
-
-            with arcpy.da.SearchCursor(os.path.join(wks(outputFeature), '{}_points_proj{}'.format(getFileName(gpxfile),Feat_shp(outputFeature))),
-                                        ['Shape@XYZ',name_desc_col]) as sc:
+            arcpy.GPXtoFeatures_conversion(gpxfile, r'in_memory\tempFile{}'.format(fName(gpxfile)))
+            with arcpy.da.SearchCursor(r'in_memory\tempFile{}'.format(fName(gpxfile)),['Shape@XYZ',name_desc_col]) as sc:
                 for row in sc:
                     if row[1] not in polygons.keys():
-                        polygons[row[1]] = [[row[0][0],row[0][1]]]
+                        polygons[row[1]] = arcpy.Array([arcpy.Point(row[0][0],row[0][1])])
                     else:
-                        polygons[row[1]].append([row[0][0],row[0][1]])
-            cleanUp(os.path.join(wks(outputFeature), '{}_points{}'.format(getFileName(gpxfile),Feat_shp(outputFeature))))
-            cleanUp(os.path.join(wks(outputFeature), '{}_points_proj{}'.format(getFileName(gpxfile),Feat_shp(outputFeature))))
-        arcpy.CreateFeatureclass_management(os.path.dirname(outputFeature), os.path.basename(outputFeature),'POLYGON','','','',coord_sys)
+                        polygons[row[1]].append(arcpy.Point(row[0][0],row[0][1]))
+            cleanUp(r'in_memory\tempFile{}'.format(fName(gpxfile)))
+
+        arcpy.CreateFeatureclass_management(os.path.dirname(outputFeature), os.path.basename(outputFeature),'POLYGON','','','',arcpy.SpatialReference(4326))
         arcpy.AddField_management(outputFeature, name_desc_col, 'TEXT')
         with arcpy.da.InsertCursor(outputFeature,['Shape@',name_desc_col]) as ic:
             for x in polygons:
-                arr = arcpy.Array([arcpy.Point(*coord)for coord in polygons[x]])
-                arr.append(arr[0])
-                plot = arcpy.Polygon(arr, coord_sys)
+                plot = arcpy.Polygon(polygons[x], arcpy.SpatialReference(4326))
                 ic.insertRow((plot, x))
-        if area_condition == 'true':
-            #Process: Compute area of various polygons
-            arcpy.SetProgressorLabel('Computing Area of{}.........'.format(os.path.basename(outputFeature)))
-            arcpy.AddMessage('Computing Area of{}.........'.format(os.path.basename(outputFeature)))
-            areaHeader = 'Area_{}'.format(area_unit.capitalize())
-            if len(areaHeader) > 10:
-                areaHeader = areaHeader[0:10]
-            arcpy.AddField_management(outputFeature, areaHeader, 'DOUBLE')
-            arcpy.CalculateField_management(outputFeature, areaHeader, '!shape.area@{}!'.format(area_unit.lower()),'PYTHON')
 
-        if RasterAttachment_condition == 'ATTACHED':
-            attachRasterFiles(Raster_Files_Location=RasterFiles_Location, Feature_name_desc_col=name_desc_col, Feature=outputFeature)
-        pass
+        if computeArea == 'true':
+            fieldName = 'Area_in_'
+            count = 0
+            while len(fieldName) != 10:
+                fieldName+=areaUnit[count]
+                count+=1
+            arcpy.AddField_management(outputFeature, fieldName, 'DOUBLE')
+            arcpy.CalculateField_management(outputFeature, fieldName, '!shape.area@{}!'.format(areaUnit.lower()),'PYTHON')
+
     except arcpy.ExecuteError as err:
         arcpy.AddError(err)
-        for gpxfile in gpxFiles:
-               if (exists(os.path.join(wks(outputFeature),'{}_points{}'.format(getFileName(gpxfile),Feat_shp(outputFeature))))):
-                arcpy.Delete_management(os.path.join(wks(outputFeature),'{}_points{}'.format(getFileName(gpxfile),Feat_shp(outputFeature))))
-               if (exists(os.path.join(wks(outputFeature),'{}_points_proj{}'.format(getFileName(gpxfile),Feat_shp(outputFeature))))):
-                arcpy.Delete_management(os.path.join(wks(outputFeature),'{}_points_proj{}'.format(getFileName(gpxfile),Feat_shp(outputFeature))))
+        try:
+            if arcpy.Exists(r'in_memory\tempFile{}'.format(fName(gpxfile))):
+                arcpy.Delete_management(r'in_memory\tempFile{}'.format(fName(gpxfile)))
+        except arcpy.ExecuteError as err2:
+            arcpy.AddError(err2)
     finally:
         arcpy.AddMessage('Cleaning up.................')
         del(polygons) #clean up
